a8a10f2d6ba82fdb7e3c451784f021be005b4d49
TheScarastic <warabhishek@gmail.com>
2017-12-29 17:57:30 +0530

    drivers: input: fingerprint: Import markw drivers Co-Authored-By: @nichcream, @me-cafebabe @SonicBSV
    [kras: Updated for ten markw bringup; code cleanup]
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/input/fingerprint/goodix_markw/netlink.c
+++ drivers/input/fingerprint/goodix_markw/netlink.c
@@ -7,11 +7,20 @@
 #include <net/netlink.h>
 
 #define NETLINK_TEST 25
-#define MAX_MSGSIZE 32
+#define MAX_MSGSIZE 4 * 1024
 int stringlength(char *s);
 void sendnlmsg(char *message);
-static int pid = -1;
+int pid;
+int err;
 struct sock *nl_sk = NULL;
+int flag = 0;
+
+struct gf_uk_channel {
+	int channel_id;
+	int reserved;
+	char buf[3*1024];
+	int len;
+};
 
 void sendnlmsg(char *message)
 {
@@ -19,14 +28,11 @@ void sendnlmsg(char *message)
 	struct nlmsghdr *nlh;
 	int len = NLMSG_SPACE(MAX_MSGSIZE);
 	int slen = 0;
-	int ret = 0;
-	if (!message || !nl_sk || !pid) {
+	if (!message || !nl_sk)
 		return ;
-	}
 	skb_1 = alloc_skb(len, GFP_KERNEL);
 	if (!skb_1) {
-		pr_err("alloc_skb error\n");
-		return;
+		printk(KERN_ERR "my_net_link:alloc_skb_1 error\n");
 	}
 	slen = strlen(message);
 	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
@@ -34,17 +40,11 @@ void sendnlmsg(char *message)
 	NETLINK_CB(skb_1).portid = 0;
 	NETLINK_CB(skb_1).dst_group = 0;
 
+	message[slen] = '\0';
 	memcpy(NLMSG_DATA(nlh), message, slen+1);
-	/* printk("my_net_link:send message %d.\n",*(char *)NLMSG_DATA(nlh)); */
 
-	ret = netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
-	if (!ret) {
-		/*kfree_skb(skb_1);*/
-		pr_err("send msg from kernel to usespace failed ret 0x%x \n", ret);
-	}
-
+	netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
 }
-
 
 void nl_data_ready(struct sk_buff *__skb)
 {
@@ -54,32 +54,25 @@ void nl_data_ready(struct sk_buff *__skb)
 	skb = skb_get (__skb);
 	if (skb->len >= NLMSG_SPACE(0)) {
 		nlh = nlmsg_hdr(skb);
-
 		memcpy(str, NLMSG_DATA(nlh), sizeof(str));
 		pid = nlh->nlmsg_pid;
-
 		kfree_skb(skb);
 	}
-
 }
-
 
 int netlink_init(void)
 {
 	struct netlink_kernel_cfg netlink_cfg;
-	memset(&netlink_cfg, 0, sizeof(struct netlink_kernel_cfg));
-
 	netlink_cfg.groups = 0;
 	netlink_cfg.flags = 0;
 	netlink_cfg.input = nl_data_ready;
 	netlink_cfg.cb_mutex = NULL;
 
-	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST,
-			&netlink_cfg);
+	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &netlink_cfg);
 
 	if (!nl_sk) {
-		pr_err("create netlink socket error\n");
-		return 1;
+	printk(KERN_ERR "my_net_link: create netlink socket error.\n");
+	return 1;
 	}
 
 	return 0;
@@ -88,10 +81,8 @@ int netlink_init(void)
 void netlink_exit(void)
 {
 	if (nl_sk != NULL) {
-		netlink_kernel_release(nl_sk);
-		nl_sk = NULL;
+		sock_release(nl_sk->sk_socket);
 	}
 
-	pr_info("self module exited\n");
+	printk("my_net_link: self module exited\n");
 }
-
